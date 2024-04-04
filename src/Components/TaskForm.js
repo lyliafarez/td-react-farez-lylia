8ca6f043271cea42c310e4cf259b52014153bfa8
lyliafarez <lylia@heliop.com>
2024-04-04 14:31:42 +0200

    Correction de l'objet tache

--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -2,48 +2,51 @@ import { useState, useEffect } from "react";
 import TaskList from "./TaskList";
 
 function TaskForm({}) {
-    const [items, setItems] = useState(() => {
-        return JSON.parse(localStorage.getItem('tasks')) || []
-      });
-    const [input, setInput] = useState("");
-   
-    const handleInput = (event)=>{
-        setInput(event.target.value)
+  const [items, setItems] = useState(() => {
+    return JSON.parse(localStorage.getItem("tasks")) || [];
+  });
+  const [input, setInput] = useState("");
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+  const addItem = () => {
+    if (input.length > 0) {
+      let new_list = items.concat({ name: input, completed: false });
+      setItems(new_list);
+      setInput("");
     }
-    const addItem = ()=>{
-        if(input.length > 0){
-            let new_list = items.concat({name:input,state:"nonCompleted"})
-            console.log(new_list)
-        setItems(new_list) //setItems([...items, word])
-        setInput('')
-        }  
-        
-     }
-    
-    
-     useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(items));
-      }, [items]);
- 
-      
-   
-    return (
-      <div data-cy="task-form" className="flex flex-col justify-center items-center my-4">
-        <div className="flex flex-row gap-2">
+  };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <div
+      data-cy="task-form"
+      className="flex flex-col justify-center items-center my-4"
+    >
+      <div className="flex flex-row gap-2">
         <input
-        data-cy="task-input"
-       onChange={handleInput}
-        value={input}
-        placeholder="Enter a task"
-        className="px-2 border border-1 border-black rounded-md"
-      />
-      <button data-cy="add-task-btn" className="px-2 py-2 rounded-md bg-blue-300" onClick={addItem}>Ajouter une tâche</button>
-     
-        </div>
-       
-      <TaskList tasks={items} key={items}/>
+          data-cy="task-input"
+          onChange={handleInput}
+          value={input}
+          placeholder="Enter a task"
+          className="px-2 border border-1 border-black rounded-md"
+        />
+        <button
+          data-cy="add-task-btn"
+          className="px-2 py-2 rounded-md bg-blue-300"
+          onClick={addItem}
+        >
+          Ajouter une tâche
+        </button>
       </div>
-    );
-  }
-  
-  export default TaskForm;
+
+      <TaskList tasks={items} key={items} />
+    </div>
+  );
+}
+
+export default TaskForm;
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,69 +1,89 @@
 import { useState, useEffect } from "react";
-function TaskList({tasks}) {
-
-const [items, setItems] = useState(tasks)
-const [filtered_items,setFilteredItems] = useState(() => {
-    return JSON.parse(localStorage.getItem('tasks')) || []
+function TaskList({ tasks }) {
+  const [items, setItems] = useState(tasks);
+  const [filtered_items, setFilteredItems] = useState(() => {
+    return JSON.parse(localStorage.getItem("tasks")) || [];
   });
 
-useEffect(() => {
-    setFilteredItems(JSON.parse(localStorage.getItem("tasks")))
+  useEffect(() => {
+    setFilteredItems(JSON.parse(localStorage.getItem("tasks")));
     localStorage.setItem("tasks", JSON.stringify(items));
-    //userInfo = JSON.parse(localStorage.getItem("userInfo"));
   }, [items]);
 
- 
+  const filterTasks = (filter) => {
+    if (filter === "Completed") {
+      let list = items.filter((item) => item.completed === true);
+      setFilteredItems(list);
+    } else if (filter === "NonCompleted") {
+      let list = items.filter((item) => item.completed === false);
+      setFilteredItems(list);
+    } else {
+      setFilteredItems(items);
+    }
+  };
+  const updateState = (index) => {
+    let item = filtered_items[index];
+    if (item.completed === true) {
+      item.completed = false;
+    } else {
+      item.completed = true;
+    }
 
-const filterTasks = (filter)=>{
-    if(filter === 'Completed'){
-        let list = items.filter((item)=>item.state === 'completed')
-       setFilteredItems(list)
-    }else if(filter === 'NonCompleted'){
-        let list = items.filter((item)=>item.state === 'nonCompleted')
-       setFilteredItems(list)
-    }else{
-       setFilteredItems(items)
-    }
- }
- const updateState = (index)=>{
-    let item = filtered_items[index]
-    if(item.state === 'completed'){
-        item.state = 'nonCompleted'
-    }else{
-        item.state = 'completed'
-    }
-   
     setItems(filtered_items);
     window.location.reload();
+  };
 
- }
-
-    return (
-     <div data-cy="task-list" className="my-5">
-         {/* filters */}
-         <div className="flex flex-row gap-2">
-         <button data-cy="filter-btn-all" className="px-2 py-1 rounded-md bg-blue-300" type="submit" onClick={(e)=>filterTasks('All')}>Tous</button>
-        <button data-cy="filter-btn-done" className="px-2 py-1 rounded-md bg-green-300" type="submit" onClick={(e)=>filterTasks('Completed')}>Complétées</button>
-        <button data-cy="filter-btn-undone" className="px-2 py-1 rounded-md bg-red-300" type="submit" onClick={(e)=>filterTasks('NonCompleted')}>Non complétées</button>
-         </div>
-         <div className="my-4 flex flex-col gap-3">
-         { filtered_items.map((item,index)=>{
-       return(
-       
-           
+  return (
+    <div data-cy="task-list" className="my-5">
+      {/* filters */}
+      <div className="flex flex-row gap-2">
+        <button
+          data-cy="filter-btn-all"
+          className="px-2 py-1 rounded-md bg-blue-300"
+          type="submit"
+          onClick={(e) => filterTasks("All")}
+        >
+          Tous
+        </button>
+        <button
+          data-cy="filter-btn-done"
+          className="px-2 py-1 rounded-md bg-green-300"
+          type="submit"
+          onClick={(e) => filterTasks("Completed")}
+        >
+          Complétées
+        </button>
+        <button
+          data-cy="filter-btn-undone"
+          className="px-2 py-1 rounded-md bg-red-300"
+          type="submit"
+          onClick={(e) => filterTasks("NonCompleted")}
+        >
+          Non complétées
+        </button>
+      </div>
+      <div className="my-4 flex flex-col gap-3">
+        {filtered_items.map((item, index) => {
+          return (
             <div data-cy="task-item" className="" key={index}>
-               
-                <button className="px-2 py-1 rounded-md w-full" style={{backgroundColor: item.state === 'completed' ? 'green':'red'}}  onClick={(e)=>{updateState(index)}}> {item.name}</button>
-                </div>
-      
-        
-       )
-      })}
-         </div>
-      
-        
-     </div>
-    );
-  }
-  
-  export default TaskList;
+              <button
+                className="px-2 py-1 rounded-md w-full"
+                style={{
+                  backgroundColor: item.completed === true ? "green" : "red",
+                }}
+                onClick={(e) => {
+                  updateState(index);
+                }}
+              >
+                {" "}
+                {item.name}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TaskList;
